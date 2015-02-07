da9624fd0396e5f341ea37ddb362dbfe9453f4d3
三代澤聡 <misirosawa@gmail.com>
2015-02-07 11:06:26 +0900

    PushのTest

--- MoneyTest.rb
+++ MoneyTest.rb
@@ -5,6 +5,6 @@ class MoneyTest < Test::Unit::TestCase
 	def testMultiplication()
 		five = Dollar.new(5)
 		five.times(2)
-		assert_equal(5, five.amount())
+		assert_equal(10, five.amount())
 	end
 end
